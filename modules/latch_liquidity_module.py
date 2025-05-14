f5b8ae3dea9bed5faff21ee65aac6e4f8e963a33
Hanz <hi@hanz.dev>
2025-05-14 14:29:45 +0700

    feat(modules): Added get_apy function

--- modules/latch_liquidity_module.py
+++ modules/latch_liquidity_module.py
@@ -2,8 +2,10 @@ from templates.liquidity_module import LiquidityModule, Token
 from typing import Dict, Optional
 from decimal import Decimal
 
+from requests import post
+
 class LatchLiquidityModule(LiquidityModule):
-    def get_amount_out(
+    def get_sell_quote(
         self, 
         pool_states: Dict, 
         fixed_parameters: Dict,
@@ -14,7 +16,7 @@ class LatchLiquidityModule(LiquidityModule):
         # Implement logic to calculate output amount given input amount
         pass
 
-    def get_amount_in(
+    def get_buy_quote(
         self, 
         pool_state: Dict, 
         fixed_parameters: Dict,
@@ -25,9 +27,39 @@ class LatchLiquidityModule(LiquidityModule):
         # Implement logic to calculate required input amount given output amount
         pass
 
+
+    def __get_apy(self, poolAddress: str) -> Decimal:
+        url = "https://savings-graphigo.prd.latch.io/query"
+
+        headers = {
+            "Content-Type": "application/json",
+        }
+
+        payload = {
+            "operationName": "GetAPYs",
+            "variables": {
+                "lsdToken": poolAddress
+            },
+            "query": "query GetAPYs($lsdToken: String!) {\n  GetAPYs(lsdToken: $lsdToken)\n}"
+        }
+
+        response = post(url, headers=headers, json=payload)
+
+        return Decimal(response.json()["data"]["GetAPYs"])
+    
     def get_apy(self, pool_state: Dict) -> Decimal:
-        # Implement APY calculation logic
-        pass
+        vaultName = pool_state["VaultName"]
+        if vaultName == "USDT":
+            poolAddress = "0xc4af68Dd5b96f0A544c4417407773fEFDc97F58d"
+        elif vaultName == "ETH":
+            poolAddress = "0xc314b8637B05A294Ae9D9C29300d5f667c748baD"
+        
+        try:
+            return self.__get_apy(poolAddress)
+        except Exception as e:
+            print(f"Error fetching APY for {vaultName} Latch Vault: {e}")
+            return Decimal(0)
+        
 
     def get_tvl(self, pool_state: Dict, token: Optional[Token] = None) -> Decimal:
         # Implement TVL calculation logic
