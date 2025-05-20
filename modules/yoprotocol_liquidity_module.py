66f347b1aa7108d8a66d92fa522713adaa22c1ca
Hanz <hi@hanz.dev>
2025-05-20 11:46:32 +0700

    feat: Added get_apy and get_tvl

--- modules/yoprotocol_liquidity_module.py
+++ modules/yoprotocol_liquidity_module.py
@@ -2,7 +2,10 @@ from templates.liquidity_module import LiquidityModule, Token
 from typing import Dict, Optional
 from decimal import Decimal
 
-class MyProtocolLiquidityModule(LiquidityModule):
+class Constant:
+    ETHER = Decimal('1e18')
+
+class YoProtocolLiquidityModule(LiquidityModule):
     def get_amount_out(
         self, 
         pool_states: Dict, 
@@ -11,8 +14,9 @@ class MyProtocolLiquidityModule(LiquidityModule):
         output_token: Token,
         input_amount: int, 
     ) -> tuple[int | None, int | None]:
-        # Implement logic to calculate output amount given input amount
-        pass
+        """
+        Returns the amount of underlying asset required to transfer to get output_amount shares in Ether denomination. (using OZ ERC4626 convertToAssets function)
+        """
 
     def get_amount_in(
         self, 
@@ -22,13 +26,36 @@ class MyProtocolLiquidityModule(LiquidityModule):
         output_token: Token,
         output_amount: int
     ) -> tuple[int | None, int | None]:
-        # Implement logic to calculate required input amount given output amount
+        """
+        Returns the amount of underlying asset received from input_amount of shares in Ether denomination. (using OZ ERC4626 convertToShares function)
+        """
         pass
 
     def get_apy(self, pool_state: Dict) -> Decimal:
-        # Implement APY calculation logic
-        pass
+        """
+        Returns the APY of the pool in decimal percentage.
+        """
+
+        currentSharePrice = Decimal(pool_state['currentSharePrice'])
+        firstSharePrice = Decimal(pool_state['firstSharePrice'])
+        uptimeDays = Decimal(pool_state['uptimeDays'])
+
+        return (currentSharePrice - firstSharePrice) / Constant.ETHER / uptimeDays * Decimal(365) * Decimal(100)
 
     def get_tvl(self, pool_state: Dict, token: Optional[Token] = None) -> Decimal:
-        # Implement TVL calculation logic
-        pass
+        """
+        Returns the total value locked (TVL) in the pool in Ether denomination.
+        """
+        
+        totalAssets = Decimal(pool_state['totalAssets'])
+
+        if token.address != pool_state['underlyingTokenAddress']:
+            raise ValueError("Token address does not match the underlying token address in the pool state.")
+        
+        tvl = 0
+        # write access is only on Base, and yoETH uses WETH as underlying token
+        if token.address == "0x4200000000000000000000000000000000000006":
+            tvl = totalAssets / Constant.ETHER
+        else:
+            tvl = totalAssets * token.reference_price / Constant.ETHER
+        return tvl
