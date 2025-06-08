184cc69e13bfd2ad8ac9acc8ac9cc5a72001e90d
Hanz <hi@hanz.dev>
2025-06-08 09:16:38 +0700

    feat: Added liquid staking primary market logic for `get_amount_out`

--- modules/tranchess_yield_liquidity_module.py
+++ modules/tranchess_yield_liquidity_module.py
@@ -3,6 +3,25 @@ from typing import Dict, Optional
 from decimal import Decimal
 
 class TranchessYieldLiquidityModule(LiquidityModule):
+    def _multiplyDecimal(self, x, y) -> int:
+        # From SafeDecimalMath.sol
+        decimals = 18
+        UNIT = 10 ** decimals
+        return (x * y) // UNIT
+    
+    def _get_multihop_result(
+        self,
+        pool_states: Dict,
+        fixed_parameters: Dict,
+        input_token: Token,
+        output_token: Token,
+        input_amount: int
+    ) -> int:
+        # Obtainable from SwapAdded(address addr0, address addr1, address swap) event
+        # it MUST be structured as this: swapMap[addr0][addr1] = swap
+        swapMap = pool_states["swapMap"]
+
+
     def get_amount_out(
         self, 
         pool_states: Dict, 
@@ -14,7 +33,45 @@ class TranchessYieldLiquidityModule(LiquidityModule):
         """
         Calculate the amount of output token received for a given input amount.
         """
-        pass
+        fee = 0
+        outputAmount = 0
+
+        if pool_states["isLiquidStaking"]:
+            if pool_states["isPrimaryMarket"]:
+                fundUnderlying = pool_states["fundUnderlying"]
+                fundEquivalentTotalQ = pool_states["fundEquivalentTotalQ"]
+                
+                if output_token.address.lower() == fixed_parameters["QUEENTokenAddress"]:
+                    # Creation
+                    if fundEquivalentTotalQ <= 0:
+                        fee = None
+                        outputAmount = None
+                    else:
+                        prec = (fundEquivalentTotalQ - 1) / fundEquivalentTotalQ
+                        minOutQ = (fundEquivalentTotalQ * (input_amount - prec)) / fundUnderlying
+                        
+                        fee = None
+                        outputAmount = int(minOutQ)
+                if input_token.address.lower() == fixed_parameters["QUEENTokenAddress"]:
+                    # Redemption
+                    resultingUnderlying = input_amount * fundUnderlying / fundEquivalentTotalQ
+                    resultingFee = self._multiplyDecimal(resultingUnderlying, fixed_parameters["QUEENRedemptionFee"])
+                    
+                    fee = resultingFee
+                    outputAmount = resultingUnderlying - resultingFee
+
+            else:
+                # Swap in secondary market
+                # nQUEEN-BNB Stable Swap: 0xfcF44D5EB5C4A03D03CF5B567C7CDe9B66Ba5773
+                pass
+        elif pool_states["isTurboAndStableFund"]:
+            pass
+        else:
+            fee = None
+            outputAmount = None
+        
+        return fee, outputAmount
+
 
     def get_amount_in(
         self, 
