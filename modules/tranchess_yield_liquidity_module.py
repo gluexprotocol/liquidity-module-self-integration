2021e8d5be556176f734553fc221752dba6b817e
Hanz <hi@hanz.dev>
2025-06-09 12:07:12 +0700

    feat: Added initial secondary market swap logic

--- modules/tranchess_yield_liquidity_module.py
+++ modules/tranchess_yield_liquidity_module.py
@@ -8,6 +8,108 @@ class TranchessYieldLiquidityModule(LiquidityModule):
         decimals = 18
         UNIT = 10 ** decimals
         return (x * y) // UNIT
+
+    def _get_swap_addy(self, swapMap: Dict, addr0: str, addr1: str) -> Optional[str]:
+        addr0_int = int(addr0.replace('0x', ''), 16)
+        addr1_int = int(addr1.replace('0x', ''), 16)
+        base, quote = (addr0, addr1) if addr0_int < addr1_int else (addr1, addr0)
+        return swapMap.get(base, {}).get(quote)
+    
+    def _get_split(
+        self,
+        pool_states: Dict,
+
+        inQ: int
+    ) -> int:
+        # Obtained from Fund.splitRatio() view function
+        splitRatio = pool_states["splitRatio"]
+
+        outB = self._multiplyDecimal(inQ, splitRatio)
+        return outB
+    
+    def _do_rebalance(
+        self,
+        pool_states: Dict,
+        amountQ: int,
+        amountB: int,
+        amountR: int,
+        index: int
+    ) -> tuple[int, int, int]:
+        # Obtainable from event RebalanceTriggered(uint256 indexed index, uint256 indexed day, uint256 navSum, uint256 navB, uint256 navROrZero, uint256 ratioB2Q, uint256 ratioR2Q, uint256 ratioBR);
+        # it's on Fund contracts
+        rebalance = pool_states["rebalances"][index]
+        newAmountQ = amountQ + self._multiplyDecimal(amountB, rebalance["ratioB2Q"]) + self._multiplyDecimal(amountR, rebalance["ratioR2Q"])
+        ratioBR = rebalance["ratioBR"]
+        newAmountB = self._multiplyDecimal(amountB, ratioBR)
+        newAmountR = self._multiplyDecimal(amountR, ratioBR)
+
+        return newAmountQ, newAmountB, newAmountR
+    
+    def _batch_rebalance(
+        self,
+        pool_states: Dict,
+
+        amountQ: int,
+        amountB: int,
+        amountR: int,
+        fromIndex: int,
+        toIndex: int
+    ) -> tuple[int, int, int]:
+        for i in range(fromIndex, toIndex):
+            amountQ, amountB, amountR = self._do_rebalance(pool_states, amountQ, amountB, amountR, i)
+        
+        return amountQ, amountB, amountR
+    
+    def _get_v2_rebalance_result(
+        self,
+        pool_states: Dict,
+        fixed_parameters: Dict,
+        latest_version: int
+    ) -> tuple[int, int]:
+        currentVersion = pool_states["currentVersion"]
+        baseBalance = pool_states["baseBalance"]
+        quoteBalance = pool_states["quoteBalance"]
+
+        newBase = 0
+        newQuote = 0
+
+        if currentVersion == latest_version:
+            newBase = baseBalance
+            newQuote = quoteBalance
+        else:
+            oldBaseBalance = baseBalance
+            oldQuoteBalance = quoteBalance
+            excessiveQ, newBase, _ = self._batch_rebalance(
+                pool_states,
+
+                0,
+                oldBaseBalance,
+                0,
+                currentVersion,
+                latest_version
+            )
+
+            if newBase < oldBaseBalance:
+                pass # TODO
+        
+        return newBase, newQuote
+
+
+    def _get_quote_out(
+        self,
+        pool_states: Dict,
+        fixed_parameters: Dict,
+        input_token: Token,
+        output_token: Token,
+        input_amount: int
+    ) -> int:
+        # Obtainable from Fund.getRebalanceSize() view function
+        rebalanceSize = pool_states["rebalanceSize"]
+        oldBase, oldQuote = self._get_v2_rebalance_result(
+            pool_states,
+            fixed_parameters,
+            rebalanceSize
+        )
     
     def _get_multihop_result(
         self,
@@ -16,12 +118,44 @@ class TranchessYieldLiquidityModule(LiquidityModule):
         input_token: Token,
         output_token: Token,
         input_amount: int
-    ) -> int:
+    ) -> Optional[tuple[list[int], list[str], list[bool]]]:
         # Obtainable from SwapAdded(address addr0, address addr1, address swap) event
         # it MUST be structured as this: swapMap[addr0][addr1] = swap
         swapMap = pool_states["swapMap"]
+        swapPath = pool_states["swapPath"]
+        # Obtainable by calling baseAddress() view function from `swap` of SwapAdded event
+        # it MUST be structured as this: baseAddresses[swap] = baseAddress
+        baseAddresses = pool_states["baseAddresses"]
+
+        # How many to swap?
+        amounts = [0] * len(swapPath)
+        # Where to swap?
+        swaps = [None] * (len(swapPath) - 1)
+        # Is it a buy or sell?
+        isBuy = [False] * (len(swapPath) - 1)
+
+        for i in range(len(swapPath) - 1):
+            swaps[i] = self._get_swap_addy(
+                swapMap,
+                swapPath[i],
+                swapPath[i + 1]
+            )
+            if swaps[i] is None:
+                return None
+            if swapPath[i] == baseAddresses[swaps[i]]:
+                # Not a buy
+                amounts[i + 1] = swaps[i].getQuoteOut(amounts[i])
+            else:
+                # Is a buy
+                isBuy[i] = True
+                amounts[i + 1] = swaps[i].getBaseOut(amounts[i])
     
-    def _get_creation_result(self, fundUnderlying: int, fundEquivalentTotalQ: int, input_amount: int) -> tuple[int | None, int | None]:
+    def _get_creation_result(
+        self,
+        fundUnderlying: int,
+        fundEquivalentTotalQ: int,
+        input_amount: int
+    ) -> tuple[int | None, int | None]:
         fee = 0
         outputAmount = 0
 
@@ -37,7 +171,13 @@ class TranchessYieldLiquidityModule(LiquidityModule):
         
         return fee, outputAmount
 
-    def _get_redemption_result(self, fundUnderlying: int, fundEquivalentTotalQ: int, redemptionFee: int, input_amount: int) -> tuple[int | None, int | None]:
+    def _get_redemption_result(
+        self,
+        fundUnderlying: int,
+        fundEquivalentTotalQ: int,
+        redemptionFee: int,
+        input_amount: int
+    ) -> tuple[int | None, int | None]:
         resultingUnderlying = input_amount * fundUnderlying / fundEquivalentTotalQ
         resultingFee = self._multiplyDecimal(resultingUnderlying, redemptionFee)
 
@@ -64,7 +204,11 @@ class TranchessYieldLiquidityModule(LiquidityModule):
                 
                 if output_token.address.lower() == fixed_parameters["QUEENTokenAddress"]:
                     # Creation
-                    fee, outputAmount = self._get_creation_result(fundUnderlying, fundEquivalentTotalQ, input_amount)
+                    fee, outputAmount = self._get_creation_result(
+                        fundUnderlying,
+                        fundEquivalentTotalQ,
+                        input_amount
+                    )
                 elif input_token.address.lower() == fixed_parameters["QUEENTokenAddress"]:
                     # Redemption
                     resultingFee, resultingUnderlying = self._get_redemption_result(
